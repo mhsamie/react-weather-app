ac1d9f3e875389103c3637892477104879234298
mhsamie <sfatima7720@gmail.com>
2022-03-19 21:25:14 +0330

    search engin first codes

---
+++ weather-progect/src/components/SearchEngin.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./Search.css";
+import axios from "axios";
+function Search() {
+  const [city, setCity] = useState("");
+  const [descript, setDescript] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function changeCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+  function displayTemp(response) {
+    setDescript(true);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+  function searchCity(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5ef53446dcb07ea2a0fb8ef5e317e310&units=metric`;
+
+    axios.get(apiUrl).then(displayTemp);
+  }
+  if (descript) {
+    return (
+      <>
+        <div className="search">
+          <form onSubmit={searchCity}>
+            <input onChange={changeCity} type="search" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+        <div className="show-temp">
+          <h3>{city}</h3>
+          <p>
+            {weather.temperature}°{" "}
+            <span>
+              <a href="#">C</a>
+            </span>{" "}
+            <span>
+              | <a href="#">F</a>
+            </span>
+          </p>
+
+          <p>{weather.description}</p>
+          <div>
+            <p>wind speed: {weather.wind} km/h</p>
+            <p>humidity: {weather.humidity}%</p>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div className="search">
+        <form onSubmit={searchCity}>
+          <input onChange={changeCity} type="search" />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
---
+++ weather-progect/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./components/Parent.css";
+import reportWebVitals from "./reportWebVitals";
+import Parent from "./components/Parent";
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Parent />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
